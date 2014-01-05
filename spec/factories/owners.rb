270618f073a274680252a6138353009b0a1a4995
Kevin <kvnkwon@gmail.com>
2014-01-04 21:01:23 -0500

    got rid of useless factories

--- spec/factories/owners.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :owner do
-    first_name "Kevin"
-    last_name "Kwon"
-    email "kwon@email.com"
-  end
-end
